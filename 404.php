ce9eb749a5a4995aa7c3da6458a4636a45680419
khairul Islam <khairuljf@gmail.com>
2017-10-29 11:32:48 +0600

    404 Design complete internal css

---
+++ 404.php
@@ -0,0 +1,24 @@
+<?php get_header(); ?>
+
+<style>
+	
+</style>
+
+<div class="site-main">
+
+
+
+<div class="container text-center" style="padding: 50px 0; font-size: 90px;">
+	<h1 style=" font-size: 90px;">404 Error <br>
+	<h1 style=" font-size: 70px;color:red;">oops! page not found 
+	 </h1>
+
+	 <h2><a href="<?php bloginfo('home'); ?>">BACK</a></h2>
+</div>
+	
+
+
+
+</div>
+
+<?php get_footer(); ?>
